9b0c06ec90523aa62748ace686664e34f0c6bc56
Tim <tim.martensson92@gmail.com>
2024-05-10 10:31:13 +0200

    Add project files.

---
+++ BlazorWebApp/BlazorWebApp/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWebApp.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    public string? UserProfileId { get; set; }
+    public UserProfile? UserProfile { get; set; }
+
+    public string? UserAddressId { get; set; }
+    public UserAddress? UserAddress { get; set; }
+
+    public bool IsExternalAccount { get; set; } = false;
+}
---
+++ BlazorWebApp/BlazorWebApp/Data/UserProfile.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorWebApp.Data;
+
+public class UserProfile
+{
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Biography { get; set; }
+    public string? ProfileImage { get; set; }
+}
