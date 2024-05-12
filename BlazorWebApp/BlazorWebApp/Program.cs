093e80d50b84193cc5449cf519d11855deec83d6
AnnSofie <annsofie.sjolander@gmail.com>
2024-05-12 10:46:43 +0200

    subscribe logic

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddHttpClient();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
