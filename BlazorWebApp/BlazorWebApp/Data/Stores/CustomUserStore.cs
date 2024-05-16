b3e07c03c812f3fe4df7a0d0a0aef29c258f44e0
Tim <tim.martensson92@gmail.com>
2024-05-16 14:29:36 +0200

    fixade lite scss men något är inte rätt med hämtningen av usern

---
+++ BlazorWebApp/BlazorWebApp/Data/Stores/CustomUserStore.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWebApp.Data.Stores;
+
+public class CustomUserStore : UserStore<ApplicationUser>
+{
+    public CustomUserStore(IDbContextFactory<ApplicationDbContext> contextFactory) : base(contextFactory.CreateDbContext())
+    {
+    }
+}
