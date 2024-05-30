2fb95ff93648c59fbdec7c6631bdde1abd26356e
tim mårtensson <tim.martensson92@gmail.com>
2024-05-30 13:34:37 +0200

    dark mode working

--- BlazorWebApp/BlazorWebApp/Program.cs
+++ BlazorWebApp/BlazorWebApp/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddHttpClient();
 builder.Services.AddSingleton<ServiceBusManager>();
+builder.Services.AddSingleton<ThemeService>();
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
---
+++ BlazorWebApp/BlazorWebApp/Services/ThemeService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebApp.Services;
+
+public class ThemeService
+{
+    private string selectedTheme = "light-mode";
+
+    public event Action? OnThemeChanged;
+    public string GetTheme()
+    {
+        if (selectedTheme == "light-mode")
+        {
+            return string.Empty;
+        }
+        else
+        {
+            return "dark-mode";
+        }
+    }
+
+    public void ToggleTheme()
+    {
+        if (selectedTheme == "light-mode")
+        {
+            selectedTheme = "dark-mode";
+        }
+        else
+        {
+            selectedTheme = "light-mode";
+        }
+    }
+}
